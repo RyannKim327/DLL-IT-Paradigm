88966f82c60daecb7d03ee3b0c2c9404631f3141
Mr.RySes <mpoprev2@email.com>
2023-07-22 16:49:23 +0800

    07-22-23 16:49 [Initial Commit]

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const _ = (element) => {
+function _ (element){
 	return document.querySelector(element)
 }
 
@@ -7,22 +7,47 @@ window.onload = () => {
 		"January", "Febuary", "March", "April", "May", "June",
 		"July", "August", "September", "October", "November", "December"
 	]
-	const today = new Date()
-	const month = today.getMonth()
-	const year = today.getFullYear()
-	const date = _("#dates")
+	let today = new Date()
+	let month = today.getMonth()
+	let year = today.getFullYear()
+	let date = _("#dates")
 
-	const totalDate = new Date(year, month + 1, 0)
-	const date2 = new Date([year, month + 1, 1])
-	const day = date2.getDay()
-	for(let i = 1; i <= totalDate.getDate() + day; i++){
-		let d = document.createElement("p")
-		if(i < (day + 1)){
-			d.textContent = ""
-		}else{
-			d.textContent = i - day
+	function reloadCalendar() {
+		date.innerHTML = ""
+		const totalDate = new Date(year, month + 1, 0)
+		const date2 = new Date([year, month + 1, 1])
+		const day = date2.getDay()
+		for(let i = 1; i <= totalDate.getDate() + day; i++){
+			let d = document.createElement("p")
+			if(i < (day + 1)){
+				d.textContent = ""
+				d.style.border = "0"
+			}else{
+				d.textContent = i - day
+			}
+			date.appendChild(d)
 		}
-		document.getElementById("dates").appendChild(d)
+		_("#month").textContent = `${months[month]} ${year}`
 	}
-	_("#month").textContent = `${months[month]} ${year}`
+	reloadCalendar()
+
+	_("#prev").onclick = () => {
+		if(month > 0){
+			month -= 1
+		}else{
+			month = 11
+			year -= 1
+		}
+		reloadCalendar()
+	}
+	
+	_("#next").onclick = () => {
+		if(month < 11){
+			month += 1
+		}else{
+			month = 0
+			year += 1
+		}
+		reloadCalendar()
+	}
 }
