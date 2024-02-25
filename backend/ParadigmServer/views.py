d1ff7d942ef35ad4f5a116593c4caa8cb9e726eb
RyannKim327 <rksesgundo123@gmail.com>
2024-02-25 18:43:17 +0800

    02-25-24 18:43 [Wala namang nabago]

--- backend/ParadigmServer/views.py
+++ backend/ParadigmServer/views.py
@@ -7,14 +7,18 @@ from .views_serializer_handler import *
 
 # Create your views here.
 
-@api_view(["GET", "POST"])
+@api_view(["POST"])
 def event(request):
 	method = request.method
-	print(method)
 	if method == "POST":
-		return HttpResponse("<h1>Hi</h1>")
-	else:
-		return HttpResponse("<h1>Error pre</h1>")
+		data = request.data
+
+		event_serializer = EventSerializer(data=data)
+		if event_serializer.is_valid():
+			event_serializer.save()
+			return HttpResponse(f"<h3>Today: {data['event_name']}</h3>")
+		else:
+			return HttpResponse("Invalid inputs")
 
 def user(request):
 	pass
