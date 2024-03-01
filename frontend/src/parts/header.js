4e5cd271a7ddeeebad5dc7ca67f9f01deff0921c
RyannKim327 <rksesgundo123@gmail.com>
2024-03-02 04:02:06 +0800

    03-02-24 04:02 [Fixed Fonts]

--- frontend/src/parts/header.js
+++ frontend/src/parts/header.js
@@ -10,8 +10,8 @@ export default function Header(){
       <header style={ head } className="flex bg-sky-500 p-[0.1rem_0.5rem]">
         <img src={ logo } className="drop-shadow-[0.25rem_0.25rem_0.25rem_rgba(0,0,0,0.5)]" alt="IT Paradigm Logo" />
         <div className="flex flex-col content-center justify-center mx-[1rem]">
-          <h3 className="fjalla">Test Header</h3>
-          <h5>Sample Subtitle</h5>
+          <h3 className="header-title fjalla">IT Paradigm</h3>
+          <h6 className="header-subtitle kode">Dalubhasaan ng Lungsod ng Lucena</h6>
         </div>
       </header>
     </div>
