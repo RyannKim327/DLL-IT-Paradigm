bbf5fbcba63a0d74b98677674ad70a078aba674c
RyannKim327 <rksesgundo123@gmail.com>
2024-03-02 11:37:01 +0800

    03-02-24 11:36 [Added Main Semi tamad na hahaha]

--- frontend/src/main/index.js
+++ frontend/src/main/index.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 
+import Main from './main.js'
 import LogReg from './login_registration_form.js'
 
 export default function Index(){
   return(
     <div>
       <Routes>
+        <Route path="/" element={<Main />} />
         <Route path="/user" element={<LogReg />}/>
       </Routes>
     </div>
---
+++ frontend/src/main/main.js
@@ -0,0 +1,7 @@
+export default function Main(){
+  return (
+    <div>
+      <p>Text</p>
+    </div>
+  )
+}
