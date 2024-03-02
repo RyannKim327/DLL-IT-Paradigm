1141678146298109c8d3a547ef7926cc8e2766d0
RyannKim327 <rksesgundo123@gmail.com>
2024-03-02 09:27:07 +0800

    03-02-24 09:27 [Fixed Footer]

--- frontend/src/parts/footer.js
+++ frontend/src/parts/footer.js
@@ -1,8 +1,8 @@
 export default function Footer(){
   return(
     <div>
-      <footer>
-        <h3>Developed by IT Paradigm</h3>
+      <footer className="fix-footer flex p-[0.1rem_1rem] bg-sky-500 w-[100%] h-[2.5rem] align-center justify-center">
+        <h3 className="footer-title">Developed by IT Paradigm</h3>
       </footer>
     </div>
   )
