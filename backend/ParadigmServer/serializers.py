c1f4506f487f61bc835e301a1638ff54d6f3f1fb
RyannKim327 <rksesgundo123@gmail.com>
2025-01-01 01:20:06 +0800

    01-01-25 01:19 [First commit of the year 2025]

--- backend/ParadigmServer/serializers.py
+++ backend/ParadigmServer/serializers.py
@@ -1,14 +1,16 @@
 from django.db.models.expressions import fields
 from rest_framework import serializers
+
 from .models import *
 
 
 class EventSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Event
-		fields = "__all__"
+    class Meta:
+        model = Event
+        fields = "__all__"
+
 
 class UserSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = User
-		fields = "__all__"
+    class Meta:
+        model = User
+        fields = "__all__"
